76ba3e6adc748a355b453f67b955e6df96c5b115
Candela Mathieu <candemathieu12@gmail.com>
2024-09-19 19:26:09 -0300

    Agrego componente nav y carrusel

---
+++ script.js
@@ -0,0 +1,6 @@
+
+  document.querySelector('.navbar-toggler').addEventListener('click', function() {
+    let navbarCollapse = document.getElementById('navbarButtonsExample');
+    navbarCollapse.classList.toggle('show');
+  });
+
